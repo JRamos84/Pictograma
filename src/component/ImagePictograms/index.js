11eb58337d9c999e0b5a082c87befe412ba428d1
JRamos84 <joseph0001@gmail.com>
2023-03-09 07:51:56 -0300

    muestra pictogramas seleccionado

--- src/component/ImagePictograms/index.js
+++ src/component/ImagePictograms/index.js
@@ -4,6 +4,7 @@ import getPictogram from 'service/getPictogram'
 import './styles.css'
 export default function ImagePictograms ({ keyword }) {
   const [pictos, setPictos] = useState([])
+  const [selectPictogram, setSelectPictogram] = useState([])
 
   useEffect(function () {
     getPictogram({ keyword })
@@ -11,12 +12,26 @@ export default function ImagePictograms ({ keyword }) {
         setPictos(ids)
       })
   }, [keyword])
+  // Revisar por que la funcion en el boton se inicia vacia al precionar por primera vez
+  const handlePictogram = (id) => {
+    setSelectPictogram(selectPictogram.concat(id))
+    console.log(selectPictogram)
+  }
 
   return (
     <div>
       {pictos.map((id) => (
-        <img key={id} src={`https://api.arasaac.org/api/pictograms/${id}`} alt={id} />
+        <div key={id} onClick={() => handlePictogram(id)}>
+          <img src={`https://api.arasaac.org/api/pictograms/${id}`} alt={id} />
+        </div>
       ))}
+      <h3>pictogramas selecionado</h3>
+      {selectPictogram.map((id) => (
+        <div key={id} onClick={() => handlePictogram(id)}>
+          <img src={`https://api.arasaac.org/api/pictograms/${id}`} alt={id} />
+        </div>
+      ))}
+
     </div>
   )
 }
--- src/service/getPictogram.js
+++ src/service/getPictogram.js
@@ -5,14 +5,14 @@ const fromApiResponseToPictograms = apiResponse => {
     const id = data._id
     return id
   })
-  //  console.log(idPictograms)
+  // console.log(idPictograms)
   return idPictograms
 }
 
 export default function getPictogram ({ keyword }) {
   if (keyword === '') keyword = 'dog'
-  const apiUrl = `https://api.arasaac.org/api/pictograms/es/bestsearch/${keyword}`
-  console.log(apiUrl)
+  const apiUrl = `https://api.arasaac.org/api/pictograms/en/bestsearch/${keyword}`
+  //  console.log(apiUrl)
   return fetch(apiUrl)
     .then((res) => res.json())
     .then(fromApiResponseToPictograms)
