db7ad1227e69c08992373811d852eb8499b2b5ba
JRamos84 <joseph0001@gmail.com>
2023-03-10 00:22:17 -0300

    selectPicto funcionando, trabajar en estética

--- src/component/SelectPicto/index.js
+++ src/component/SelectPicto/index.js
@@ -2,15 +2,20 @@ import React, { useContext } from 'react'
 import SelectContext from 'context/selectContext'
 import './styles.css'
 export default function SelectPicto () {
-  const { selectPictogram } = useContext(SelectContext)
-
+  const { selectPictogram, setSelectPictogram } = useContext(SelectContext)
+  const handleColor = ({ id, status }) => {
+    const newList = [...selectPictogram]
+    const artwork = newList.find(a => a.id === id)
+    artwork.status = !status
+    setSelectPictogram(newList)
+  }
   return (
     <div className='select-picto'>
 
       {selectPictogram.map(({ id, status }) => (
         <div className='picto' key={id}>
           <img src={`https://api.arasaac.org/api/pictograms/${id}`} alt={id} />
-          <div onClick={() => !status} style={status ? { background: 'blue' } : { background: 'white' }}>color</div>
+          <div onClick={() => handleColor({ id, status })} style={status ? { background: 'blue' } : { background: 'white' }}>color</div>
         </div>
       ))}
     </div>
